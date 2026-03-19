799a09d4bee2577b6e3b7fffd3e1d1f043d3c2b1
makesoundBetter <makesoundbetter@gmail.com>
2026-03-19 20:13:59 +0300

    Disable pinch-to-zoom on iOS

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Блокировка зума (pinch-to-zoom) на iOS
+document.addEventListener('touchmove', (e) => {
+  if (e.touches.length > 1) e.preventDefault();
+}, { passive: false });
+
+document.addEventListener('gesturestart', (e) => e.preventDefault(), { passive: false });
+document.addEventListener('gesturechange', (e) => e.preventDefault(), { passive: false });
+
 // Telegram WebApp инициализация
 const tg = window.Telegram?.WebApp;
 if (tg) {
